8892fce555a9b64c5460a0b7b153b580371b2a17
charzhao <charzhao@SGLEN0802000164>
2023-11-04 19:34:30 +0800

    copy from GlobeDemo

---
+++ GlobeDemo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using GlobeDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GlobeDemo.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+        [HttpGet]
+        public JsonResult Add(int x, int y)
+        {
+            return Json(new { result = x + y });
+        }
+
+    }
+}
---
+++ GlobeDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GlobeDemo.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
